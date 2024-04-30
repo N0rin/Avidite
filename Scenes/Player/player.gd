274539d1594228e7baffef80fea42cc70900f7c8
Norin <georgl97@gmx.de>
2024-04-30 14:53:47 +0200

    Small Code Clean

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -11,7 +11,7 @@ extends CharacterBody2D
 @export var jump_peak_factor = 0.5
 @export_category("Assists")
 @export var jump_buffer = 0.2
-@export var koyote_time = 3.2
+@export var koyote_time = 0.2
 
 var air_time = 0
 var peak_time = 0
@@ -25,6 +25,7 @@ func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta * get_gravity_factor(delta)
+		air_time += delta 
 		fall_of_time += delta
 
 	if is_on_floor():
@@ -56,14 +57,12 @@ func _physics_process(delta):
 func get_gravity_factor(delta) -> float:
 	var gravity_factor = 1
 	
-	if not is_on_floor():
-		air_time += delta 
-		if air_time > 0.5 * jump_duration * (1-jump_peak_portion):
-			if velocity.y < 0:
-				peak_time += delta
-			else:
-				peak_time -= delta
-		if peak_time > 0:
+	if air_time > 0.5 * jump_duration * (1-jump_peak_portion):
+		if velocity.y < 0:
+			peak_time += delta
+		else:
+			peak_time -= delta
+	if peak_time > 0:
 			gravity_factor = jump_peak_factor
 	
 	return gravity_factor
